c3472f81f1ef9cf3f80f8018a422901ef7bff8cf
Forrest Murray <FMurray@gmail.com>
2021-05-29 11:49:00 -0400

    changes

---
+++ src/components/status/StatusPanel.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="showModal = true">Show Modal</button>
+        <!-- use the modal component, pass in the prop -->
+        <Modal v-if="showModal" @close="showModal = false">
+            <WakatimePlugin/>
+        </Modal>
+    </div>
+</template>
+
+<script>
+import WakatimePlugin from "./WakatimePlugin.vue";
+import Modal from "../ui/Modal.vue";
+export default {
+  name: "StatusPanel",
+  components: {
+    WakatimePlugin,
+    Modal
+  },
+  data() {
+    showModal: false;
+  },
+  methods: {
+    log(e) {
+      console.log(e);
+    }
+  }
+};
+</script>
---
+++ src/components/status/WakatimePlugin.vue
@@ -0,0 +1,12 @@
+<template>
+    <figure>
+        <embed src="https://wakatime.com/share/@93b0c8f9-e451-4f34-86e6-db4eaaa9e90f/32b78b85-e607-4588-b7cc-6984d42cb731.svg"></embed>
+    </figure>
+</template>
+
+<script>
+export default {
+  name: "WakatimePlugin"
+};
+</script>
+
